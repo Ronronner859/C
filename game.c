fc94582a557ec5048e9f0f025126a680ab8cbc17
Mr.Chnn <83115098+Ronronner859@users.noreply.github.com>
2022-01-13 20:44:11 +0800

    三子棋游戏代码
    C语言综合训练

--- game.c
+++ game.c
@@ -1,10 +1,16 @@
-
-#include<stdio.h>
 #define ROW 3
 #define COL 3
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
 void initboard(char board[ROW][COL], int row, int col);
 void dispalyboard(char board[ROW][COL], int row, int col);
 void Playermove(char board[ROW][COL], int row, int col);
+void Competermove(char board[ROW][COL], int row, int col);
+char Iswiner(char board[ROW][COL], int row, int col);
+int Isfull(char board[ROW][COL], int row, int col);
 
 void menu()
 {
@@ -15,15 +21,46 @@ void menu()
 
 void game()
 {
+	char ret = 0;
 	char board[ROW][COL] = { 0 };
 	//初始化棋盘
 	initboard(board, ROW, COL);
 	//打印棋盘
 	dispalyboard(board, ROW, COL);
 	//下棋
-	//玩家下
-	Playermove(board, ROW, COL);
-	dispalyboard(board, ROW, COL);
+	while (1)//对弈
+	{
+		//玩家下
+		Playermove(board, ROW, COL);
+		dispalyboard(board, ROW, COL);
+
+		ret = Iswiner(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		//电脑下棋
+		Competermove(board, ROW, COL);
+		dispalyboard(board, ROW, COL);
+		ret = Iswiner(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		
+	}
+	if (ret == '*')
+	{
+		printf("电脑赢！\n");
+	}
+	else if (ret == '#')
+	{
+		printf("玩家赢！\n");
+	}
+	else
+	{
+		printf("平局！\n");
+	}
 }
 
 void initboard(char board[ROW][COL], int row, int col)
@@ -70,6 +107,76 @@ void dispalyboard(char board[ROW][COL], int row, int col)
 	}
 }
 
+void Competermove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("电脑走:> \n");
+
+	while (1)
+	{
+		x = rand() % row;
+		y = rand() % col;
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '*';
+			break;
+		}
+	}
+}
+
+char Iswiner(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for ( i = 0; i < row; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')
+		{
+			return board[i][0];
+		}
+	}
+	for ( i = 0; i < col; i++)
+	{
+		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != ' ')
+		{
+			return board[0][i];
+		}
+	}
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	{
+		return board[0][0];
+	}
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ')
+	{
+		return board[0][0];
+	}
+	if (1 == Isfull(board, ROW, COL))
+	{
+		return 'Q';
+	}
+	else
+	{
+		return 'C';
+	}
+}
+
+int Isfull(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for ( i = 0; i < row; i++)
+	{
+		for ( j = 0; j <col; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;//满了
+			}
+		}
+	}
+	return 1;//没满
+}
 void Playermove(char board[ROW][COL], int row, int col)
 {
 	int x = 0;
@@ -98,8 +205,6 @@ void Playermove(char board[ROW][COL], int row, int col)
 	}
 }
 
-
-
 /*void dispalyboard(char board[ROW][COL], int row, int col)
 {
 	int i;
@@ -114,9 +219,11 @@ void Playermove(char board[ROW][COL], int row, int col)
 
 	}
 }*/
+
 void test()
 {
 	int input = 0;
+	srand((unsigned int)time(NULL));
 	do
 	{
 		menu();
@@ -140,8 +247,6 @@ void test()
 	} while (input);
 
 }
-
-
 
 int main()
 {
